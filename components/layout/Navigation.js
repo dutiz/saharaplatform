b2ef1aa745b9aa69338fb2d3838b7a44b050d74f
dutiz <zaimidaut@gmail.com>
2022-06-21 01:07:03 +0200

    navigation update

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { useState } from 'react'
 import SVG from 'react-inlinesvg'
+import Button from '../Button'
 
 const NavItem = ({ children, href }) => {
   return (
@@ -21,8 +22,12 @@ export default function Navigation() {
     <div className="bg-white shadow-lg flex items-center">
       <div className="container">
         <div className="row py-6 justify-between">
-          <div className="col-6">
-            <p className="font-bold text-gray-800 text-2xl">Next Starter</p>
+          <div className="col-4">
+            <Link href="/">
+              <a>
+                <SVG src='../svg/logo.svg' className='fill-current w-20 h-20'/>
+              </a>
+            </Link>
           </div>
           <div className="col-2 md:hidden block">
             <label htmlFor="menu-toggle" className="cursor-pointer">
@@ -35,14 +40,19 @@ export default function Navigation() {
           </div>
 
           <div
-            className={`md:col-6 md:flex justify-center md:justify-end ${
+            className={`md:col-4 md:flex justify-center ${
               menu ? ' flex' : ' hidden'
             }`}
           >
             <ul className="flex w-full p-5 flex-col md:w-auto md:p-0 md:flex-row items-center">
               <NavItem href="/">Home</NavItem>
               <NavItem href="/about">About</NavItem>
+              <NavItem href="/menu">Menu</NavItem>
+              <NavItem href="/admin">Admin</NavItem>
             </ul>
+          </div>
+          <div className="md:col-4 md:flex items-center justify-end">
+            <Button islink href="tel:0038972605415">0038972605415</Button>
           </div>
         </div>
       </div>
