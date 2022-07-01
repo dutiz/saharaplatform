193b82f40da1f0cabe6b10fc28dc28ec04ccbdf4
dutiz <zaimidaut@gmail.com>
2022-07-01 17:09:43 +0200

    menusection updated

--- components/sections/MenuSection.js
+++ components/sections/MenuSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../Button'
 import MenuItem from '../MenuItem'
 
-export default function MenuSection() {
+export default function MenuSection({ menuList }) {
   return (
     <div className="bg-blue-100">
       <div className="container py-16">
@@ -10,18 +10,20 @@ export default function MenuSection() {
           <div className="col-12">
             <h1 className="text-center text-white font-extrabold text-5xl">Menu</h1>
           </div>
-          </div>
-          <div className="row">
-            <MenuItem />
-          </div>
-          <div className="row">
-            <div className="col-12 text-center">
-              <Button islink className="mt-8" href="/menu">
-                For More
-              </Button>
-            </div>
+        </div>
+        <div className="row">
+          {menuList.slice(0, 6).map((menu) => (
+            <MenuItem key={menu._id} menu={menu} />
+          ))}
+        </div>
+        <div className="row">
+          <div className="col-12 text-center">
+            <Button islink className="mt-8" href="/menu">
+              For More
+            </Button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
