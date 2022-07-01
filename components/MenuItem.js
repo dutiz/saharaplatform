c17de0b2498e059025b052a7d5cffc5c83648b89
dutiz <zaimidaut@gmail.com>
2022-07-01 17:08:33 +0200

    updating menuitem component

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -2,25 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import Button from './Button'
 
-export default function MenuItem() {
+export default function MenuItem({ menu }) {
   return (
-    <div className="lg:col-6 mt-8 bg-gray-300 p-8 shadow-lg">
-      <div className="row justify-center ">
-        <div className="md:col-6 flex flex-col items-center">
-          <Image src="/images/products/pizza.png" width={208} height={208} alt="pizza-santorini" />
-        </div>
-        <div className="lg:col-6">
-          <h2 className="font-extrabold text-3xl mt-3 lg:mt-0">Santorini</h2>
-          <p className="mt-3">
-            Red Sauce, Mozzarella Cheese, Chicken, Topped with Bruschetta Tomatoes, Red Onions,
-            Kalamata Olives, and Feta Cheese
-          </p>
-          <div className="mt-3 row items-center">
-            <div className="col-6">
-              <p className="font-semibold text-2xl">$ 7.50</p>
-            </div>
-            <div className="col-6">
-              <Button>Order</Button>
+    <div className="lg:col-6 mt-8">
+      <div className="bg-gray-300 p-8 shadow-lg">
+        <div className="row justify-center ">
+          <div className="md:col-6 flex flex-col items-center">
+            <Image src={menu.img} width={208} height={208} alt="pizza-santorini" />
+          </div>
+          <div className="lg:col-6">
+            <h2 className="font-extrabold text-3xl mt-3 lg:mt-0">{menu.title}</h2>
+            <p className="mt-3">{menu.desc}</p>
+            <div className="mt-3 row items-center">
+              <div className="col-6">
+                <p className="font-semibold text-2xl">${menu.prices[0]}</p>
+              </div>
+              <div className="col-6">
+                <Button>Order</Button>
+              </div>
             </div>
           </div>
         </div>
