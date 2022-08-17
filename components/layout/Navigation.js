7174e3b917e7e18dbf2dc0ba27e55b7b1da9190f
dutiz <zaimidaut@gmail.com>
2022-08-18 01:52:37 +0200

    Update Navigation

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useState } from 'react'
+import { useSelector } from 'react-redux/'
 import SVG from 'react-inlinesvg'
 import Button from '../Button'
 
@@ -17,6 +18,7 @@ const NavItem = ({ children, href }) => {
 
 export default function Navigation() {
   const [menu, setMenu] = useState(false)
+  const quantity = useSelector((state) => state.cart.quantity)
 
   return (
     <div className="bg-white shadow-lg flex items-center">
@@ -25,7 +27,7 @@ export default function Navigation() {
           <div className="col-4">
             <Link href="/">
               <a>
-                <SVG src='../svg/logo.svg' className='fill-current w-20 h-20'/>
+                <SVG src="../svg/logo.svg" className="fill-current w-20 h-20" />
               </a>
             </Link>
           </div>
@@ -39,11 +41,7 @@ export default function Navigation() {
             </label>
           </div>
 
-          <div
-            className={`md:col-4 md:flex justify-center ${
-              menu ? ' flex' : ' hidden'
-            }`}
-          >
+          <div className={`md:col-4 md:flex justify-center ${menu ? ' flex' : ' hidden'}`}>
             <ul className="flex w-full p-5 flex-col md:w-auto md:p-0 md:flex-row items-center">
               <NavItem href="/">Home</NavItem>
               <NavItem href="/about">About</NavItem>
@@ -52,7 +50,19 @@ export default function Navigation() {
             </ul>
           </div>
           <div className="md:col-4 md:flex items-center justify-end">
-            <Button islink href="tel:0038972605415">0038972605415</Button>
+            <div className='relative mr-5'>
+              <Link href="/cart" passHref>
+                <a className="">
+                  <SVG src="../svg/cart.svg" className=" fill-current w-9 h-9 " />
+                  <div className="absolute -right-2 -top-2  w-5 h-5 bg-orange-500 p-1 flex items-center justify-center font-bold text-white rounded-xl">
+                    {quantity}
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <Button islink href="tel:0038972605415">
+              0038972605415
+            </Button>
           </div>
         </div>
       </div>
