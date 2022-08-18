70e514460443891c604aae092ffeeee86000312d
dutiz <zaimidaut@gmail.com>
2022-08-18 02:04:32 +0200

    adding tables page and tables api

---
+++ pages/api/tables/index.js
@@ -0,0 +1,26 @@
+import dbConnect from 'utils/mongo'
+import Table from 'models/Table'
+
+export default async function handler(req, res) {
+  const { method } = req
+
+  dbConnect()
+
+  if (method === 'GET') {
+    try {
+      const tables = await Table.find()
+      res.status(200).json(tables)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+
+  if (method === 'POST') {
+    try {
+      const table = await Table.create(req.body)
+      res.status(201).json(table)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+}
