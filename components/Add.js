11522a0aa29cf24b48a83523a3bd053e618221a2
miftarienis <enismiftari2@outlook.com>
2022-09-02 23:40:35 +0200

    add: add products not finished

---
+++ components/Add.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function Add = () => {
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
