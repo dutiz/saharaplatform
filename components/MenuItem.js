7e1e18aeca645f8268be8306fc4ff9773001feb5
dutiz <zaimidaut@gmail.com>
2022-10-28 02:13:18 +0200

    deleted a at menuitem

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -8,25 +8,23 @@ export default function MenuItem({ menu, href }) {
     <div className="lg:col-6 mt-8">
       <div className="bg-gray-300 p-8 shadow-lg">
         <Link href={href}>
-          <a>
-            <div className="row justify-center ">
-              <div className="md:col-6 flex flex-col items-center">
-                <Image src={menu.img} width={208} height={208} alt="pizza-santorini" />
-              </div>
-              <div className="lg:col-6">
-                <h2 className="font-extrabold text-3xl mt-3 lg:mt-0">{menu.title}</h2>
-                <p className="mt-3">{menu.desc}</p>
-                <div className="mt-3 row items-center">
-                  <div className="col-6">
-                    <p className="font-semibold text-2xl">${menu.prices[0]}</p>
-                  </div>
-                  <div className="col-6">
-                    <Button>Order</Button>
-                  </div>
+          <div className="row justify-center ">
+            <div className="md:col-6 flex flex-col items-center">
+              <Image src={menu.img} width={208} height={208} alt="pizza-santorini" />
+            </div>
+            <div className="lg:col-6">
+              <h2 className="font-extrabold text-3xl mt-3 lg:mt-0">{menu.title}</h2>
+              <p className="mt-3">{menu.desc}</p>
+              <div className="mt-3 row items-center">
+                <div className="col-6">
+                  <p className="font-semibold text-2xl">${menu.prices[0]}</p>
+                </div>
+                <div className="col-6">
+                  <Button>Order</Button>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
         </Link>
       </div>
     </div>
