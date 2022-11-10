fa8529dd533b4e714496312bd268af053916aa09
dutiz <zaimidaut@gmail.com>
2022-11-10 01:33:25 +0100

    Some fixes at dashboard cart

--- components/IncomeDashboardCart.js
+++ components/IncomeDashboardCart.js
@@ -13,15 +13,18 @@ import SVG from 'react-inlinesvg'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler)
 
-export default function DashboardCart({ svg, data, children }) {
+export default function IncomeDashboardCart({ svg, data, children }) {
   const months = Array.from({ length: new Date().getMonth() }, (_, i) => i + 1)
   const arr = []
-
+  var total = 0
+  data.forEach((element) => {
+    total = total + element.total
+  })
   months.forEach((month) => {
     var count = 0
     data.forEach((element) => {
       if (new Date(element.createdAt).getMonth() === month) {
-        count = count + 1
+        count = count + element.total
       }
     })
     arr.push(count)
@@ -32,6 +35,24 @@ export default function DashboardCart({ svg, data, children }) {
     plugins: {
       legend: {
         display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            let label = context.dataset.label || ''
+
+            if (label) {
+              label += ': '
+            }
+            if (context.parsed.y !== null) {
+              label += new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(context.parsed.y)
+            }
+            return label
+          },
+        },
       },
     },
     elements: {
@@ -71,7 +92,9 @@ export default function DashboardCart({ svg, data, children }) {
       <div className="rounded-md  bg-white p-3">
         <div className="flex flex-row items-center">
           <div className="col-6  ml-3">
-            <p className="text-2xl">{data.length}</p>
+            <p className="text-2xl">
+              {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total)}
+            </p>
             <p>{children}</p>
           </div>
           <div className="col-4 flex flex-col items-end">
