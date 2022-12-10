91918f042feac6ea7201fc1e962eeac0e962c497
dutiz <zaimidaut@gmail.com>
2022-12-10 16:13:29 +0100

    useUrl added

---
+++ utils/useUrl.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+
+function useUrl() {
+  const [url, setUrl] = useState(null)
+
+  useEffect(() => [setUrl(window.location.origin)], [])
+  return url
+}
+
+export default useUrl
